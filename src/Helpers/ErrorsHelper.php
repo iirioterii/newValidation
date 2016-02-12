8c583fb4d53c04d011df65b325e188e7b085f7c0
iirioterii <yuriyreva@gmail.com>
2016-02-12 16:49:27 +0200

    add ErrrosHelper

---
+++ src/Helpers/ErrorsHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Rioter\Validation\Error;
+
+
+class ErrorsHelper
+{
+    public function checkErrors()
+    {
+
+    }
+
+    public function outputErrors()
+    {
+
+    }
+
+    public function gerErrors()
+    {
+
+    }
+
+
+}
